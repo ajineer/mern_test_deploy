ff12ca87e1d25647560edfb307aab4a2e32c19cf
Mark <ajineer@outlook.com>
2024-03-24 14:58:21 -0600

    following new tutorial from beginning.

--- backend/config/allowedOrigins.js
+++
@@ -1,3 +0,0 @@
-const allowedOrigins = ["https://technotes.onrender.com"];
-
-module.exports = allowedOrigins;
--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -7,11 +7,11 @@ const errorHandler = (err, req, res, next) => {
   );
   console.log(err.stack);
 
-  const status = res.statusCode ? res.statusCode : 500; // server error
+  const status = res.statusCode ? res.statusCode : 500;
 
   res.status(status);
 
-  res.json({ message: err.message, isError: true });
+  res.json({ message: err.message });
 };
 
 module.exports = errorHandler;
