6e4d66a610537721a16fa74067b75f71204978aa
Mark <ajineer@outlook.com>
2024-03-25 20:37:07 -0600

    made the Note component as well as the User component

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,7 +8,7 @@ import { Provider } from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider>
+    <Provider store={store}>
       <BrowserRouter>
         <Routes>
           <Route path='/*' element={<App/>} />
